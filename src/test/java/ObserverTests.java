fc187c5ea5c34e7391e90a8b3290cf00bbd98197
Aidan Pecorale <alpecorale@wpi.edu>
2020-02-22 21:07:18 -0500

    observer test

--- src/test/java/ObserverTests.java
+++ src/test/java/ObserverTests.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertEquals;
 import edu.wpi.cs3733.entity.CaesarCipher;
+import edu.wpi.cs3733.entity.ElbonianCipher;
 import edu.wpi.cs3733.entity.Message;
 import org.junit.Test;
 
@@ -12,11 +13,18 @@ public class ObserverTests {
   @Test
   public void notifierTests1(){
     Message mess = new Message();
+    CaesarCipher caes = new CaesarCipher();
+    ElbonianCipher elbo = new ElbonianCipher();
+    mess.register(caes);
+    mess.register(elbo);
+    caes.setText("not hello");
+    elbo.setText("not hello");
     mess.setText("hello");
-    CaesarCipher cipher = new CaesarCipher();
-    cipher.setText("hi");
-    mess.notifyObservers();
-    assertEquals(mess.getText(), cipher.getText());
+    assertEquals("hello", mess.getText());
+    assertEquals("czggj", caes.getText());
+    assertEquals("0805121215", elbo.getText());
   }
+
+
 
 }
